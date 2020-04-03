a3301bdcf357c09466578902d23c84f6c8cb795f
hwgyuhyeon <57509844+hwgyuhyeon@users.noreply.github.com>
2020-04-03 14:00:36 +0900

    Update size.c

--- size.c
+++ size.c
@@ -11,9 +11,9 @@ int main(void){
 	int **x; //이중 포인터 변수 선언
 
 	printf("[-----[황규현] [2019038071]-----]\n");
-	printf("sizeof(x) = %lu\n",sizeof(x)); //포인터 변수의 크기 - 8바이트
-	printf("sizeof(*x) = %lu\n",sizeof(*x)); //포인터 변수의 크기 - 8바이트
-	printf("sizeof(**x) = %lu\n",sizeof(**x)); //포인터 변수가 가리키는 값의 크기 - 4
+	printf("sizeof(x) = %lu\n",sizeof(x)); //포인터 변수의 크기(즈소를 저장함) - 8바이트
+	printf("sizeof(*x) = %lu\n",sizeof(*x)); //포인터 변수의 크기(주소를 저장함) - 8바이트
+	printf("sizeof(**x) = %lu\n",sizeof(**x)); //포인터 변수가 가리키는 값의 크기(integer 값을 저장) - 4
 
 	return 0;
 }
