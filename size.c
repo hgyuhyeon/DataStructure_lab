79131d1d527503c4b3454e3a9c204b5bcd51a65e
hwgyuhyeon <57509844+hwgyuhyeon@users.noreply.github.com>
2020-04-03 13:47:11 +0900

    Create size.c

---
+++ size.c
@@ -0,0 +1,19 @@
+/*
+ * size.c
+ *
+ *  Created on: 3 Apr 2020
+ *      Author: ghh
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int **x; //이중 포인터 변수 선언
+
+	printf("[-----[황규현] [2019038071]-----]\n");
+	printf("sizeof(x) = %lu\n",sizeof(x)); //포인터 변수의 크기 - 8바이트
+	printf("sizeof(*x) = %lu\n",sizeof(*x)); //포인터 변수의 크기 - 8바이트
+	printf("sizeof(**x) = %lu\n",sizeof(**x)); //포인터 변수가 가리키는 값의 크기 - 4
+
+	return 0;
+}
